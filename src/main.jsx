0f2345abaa78c8e1682313aae58017aee083576f
enyichiaagu <enyichiaagu@gmail.com>
2022-12-30 19:15:29 +0100

    Installed Material UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import { CssBaseline } from '@mui/material'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Fragment>
+      <CssBaseline/>
+      <App />
+    </Fragment>
   </React.StrictMode>,
 )
