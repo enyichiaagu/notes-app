6de3bc03c60334a7417da3578861ba0c696229f1
enyichiaagu <enyichiaagu@gmail.com>
2023-01-01 17:38:33 +0100

    Removed host from package.json and added logout button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Fragment } from 'react'
-import { CssBaseline, AppBar, Toolbar, Typography } from '@mui/material'
+import { CssBaseline, AppBar, Toolbar, Typography, Button } from '@mui/material'
 
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
@@ -12,9 +12,11 @@ function App() {
       <CssBaseline />
       <AppBar>
         <Toolbar>
-          <Typography variant='h6' component='div'>Notes App</Typography>
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>Notes App</Typography>
+          <Button color='inherit'>Logout</Button>
         </Toolbar>
       </AppBar>
+
     </Fragment>
   )
 }
