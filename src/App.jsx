f8bd117ecd355f5f8ec96ba844c0a1936d3cc99b
enyichiaagu <enyichiaagu@gmail.com>
2023-01-01 16:51:36 +0100

    Added Material UI and Appbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { Fragment } from 'react'
+import { CssBaseline, AppBar, Toolbar, Typography } from '@mui/material'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 function App() {
   return (
     <Fragment>
-      <h1>Hello World</h1>
+      <CssBaseline />
+      <AppBar>
+        <Toolbar>
+          <Typography variant='h6' component='div'>Notes App</Typography>
+        </Toolbar>
+      </AppBar>
     </Fragment>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,9 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import { CssBaseline } from '@mui/material'
-
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Fragment>
-      <CssBaseline/>
-      <App />
-    </Fragment>
+    <App />
   </React.StrictMode>,
 )
