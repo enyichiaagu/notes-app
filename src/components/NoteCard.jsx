b6a63c8d0d9ae55856bc7cc8319767a00215b490
enyichiaagu <enyichiaagu@gmail.com>
2023-01-02 15:44:48 +0100

    Designed proper card format

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,12 +1,12 @@
-import { Button, Card, CardActionArea, CardActions, CardContent, CardHeader, Typography } from '@mui/material'
+import { Button, Card, CardActionArea, CardActions, CardContent, Typography } from '@mui/material'
 
 function NoteCard() {
     return (
-        <Card sx={{ width: '100%', height: '20rem' }}>
+        <Card sx={{ width: '100%' }}>
             <CardActionArea>
                 <CardContent>
                     <Typography variant='h6' component='header' gutterBottom>Random Title</Typography>
-                    <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, ad eaque voluptatem tempora adipisci aliquam eius amet dolor rerum laborum consequatur accusantium? Sequi, saepe! Recusandae, totam? Ab blanditiis sapiente laudantium maiores</Typography>
+                    <Typography height='10rem' overflow='hidden'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quas, debitis commodi culpa voluptas neque eos impedit deserunt voluptatem soluta perspiciatis labore odit nam iusto! Nam itaque, iure, quisquam quis eos hic quaerat expedita exercitationem nulla tenetur, laudantium reiciendis officia obcaecati? Eos debitis minima eaque earum, praesentium dolorem necessitatibus fuga rem dignissimos beatae assumenda autem id reiciendis iure accusamus soluta est, non iste. Tempore, enim iste. Dolores voluptatibus enim animi nisi facilis ducimus, labore, beatae corrupti ullam explicabo ipsum quaerat provident soluta assumenda voluptates vitae molestiae! Ullam, ducimus reprehenderit dolor autem libero minus optio nihil illum veritatis quibusdam non placeat earum obcaecati amet reiciendis iusto!</Typography>
                 </CardContent>
             </CardActionArea>
             <CardActions>
