b8b663034187d83ac3cb60d2eff780dd02a34e6d
enyichiaagu <enyichiaagu@gmail.com>
2023-01-03 18:48:36 +0100

    Combined all card related components into one

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Fragment } from 'react'
 import { CssBaseline, AppBar, Toolbar, Typography, Button, Container, Box, Grid, Paper } from '@mui/material'
 
 import NoteCard from './components/NoteCard'
-import AddNote from './components/AddNote'
 
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
@@ -10,6 +9,10 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
 function App() {
+  
+  const title = 'Random Title'
+  const text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quas, debitis commodi culpa voluptas neque eos impedit deserunt voluptatem soluta perspiciatis labore odit nam iusto! Nam itaque, iure, quisquam quis eos hic quaerat expedita exercitationem nulla tenetur, laudantium reiciendis officia obcaecati? Eos debitis minima eaque earum, praesentium dolorem necessitatibus fuga rem dignissimos beatae assumenda autem id reiciendis iure accusamus soluta est, non iste. Tempore, enim iste. Dolores voluptatibus enim animi nisi facilis ducimus, labore, beatae corrupti ullam explicabo ipsum quaerat provident soluta assumenda voluptates vitae molestiae! Ullam, ducimus reprehenderit dolor autem libero minus optio nihil illum veritatis quibusdam non placeat earum obcaecati amet reiciendis iusto!'
+
   return (
     <Fragment>
       <CssBaseline />
@@ -22,12 +25,8 @@ function App() {
       <Container maxWidth='lg' component='main'  sx={{ py: 5 }}>
         <Typography variant='h4' component='h2'>Notes</Typography>
         <Grid container spacing={5} sx={{ py: 5 }}>
-          <Grid item lg={4}><AddNote /></Grid>
-          <Grid item lg={4}><NoteCard /></Grid>
-          <Grid item lg={4}><NoteCard /></Grid>
-          <Grid item lg={4}><NoteCard /></Grid>
-          <Grid item lg={4}><NoteCard /></Grid>
-          <Grid item lg={4}><NoteCard /></Grid>
+          <Grid item lg={4}><NoteCard add /></Grid>
+          <Grid item lg={4}><NoteCard title={title} text={text} /></Grid>
         </Grid>
       </Container>
     </Fragment>
--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,10 +1,8 @@
 import { Fragment, useState } from 'react'
 
-import { Button, Card, CardActionArea, CardActions, CardContent, Typography } from '@mui/material'
+import { Button, Card, CardActionArea, CardActions, CardContent, Dialog, DialogActions, DialogContent, DialogTitle, SvgIcon, TextField, Typography } from '@mui/material'
 
-import EditNoteModal from './EditNoteModal'
-
-function NoteCard() {
+function NoteCard({ add, title='', text='' }) {
 
     const [ open, setOpen ] = useState(false)
 
@@ -13,19 +11,60 @@ function NoteCard() {
 
     return (
         <Fragment>
-            <Card sx={{ width: '100%' }}>
-                <CardActionArea onClick={openModal}>
-                    <CardContent>
-                        <Typography variant='h6' component='header' gutterBottom>Random Title</Typography>
-                        <Typography height='10rem' overflow='hidden'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quas, debitis commodi culpa voluptas neque eos impedit deserunt voluptatem soluta perspiciatis labore odit nam iusto! Nam itaque, iure, quisquam quis eos hic quaerat expedita exercitationem nulla tenetur, laudantium reiciendis officia obcaecati? Eos debitis minima eaque earum, praesentium dolorem necessitatibus fuga rem dignissimos beatae assumenda autem id reiciendis iure accusamus soluta est, non iste. Tempore, enim iste. Dolores voluptatibus enim animi nisi facilis ducimus, labore, beatae corrupti ullam explicabo ipsum quaerat provident soluta assumenda voluptates vitae molestiae! Ullam, ducimus reprehenderit dolor autem libero minus optio nihil illum veritatis quibusdam non placeat earum obcaecati amet reiciendis iusto!</Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button onClick={openModal}>Edit</Button>
-                    <Button>Delete</Button>
-                </CardActions>
-            </Card>
-            <EditNoteModal open={open} close={closeModal} />
+            
+            {
+                (add) ?
+
+                    <Card sx={{ width: '100%', height: '100%', textAlign: 'center' }}>
+                        <CardActionArea sx={{ height: '100%' }} onClick={openModal}>
+                            <SvgIcon color='primary' sx={{ fontSize: '6rem' }}>
+                                <path d="M0 0h24v24H0z" fill="none"/><path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
+                            </SvgIcon>
+                            <br />
+                            <Typography variant='button' color='primary'>Create new note</Typography>
+                        </CardActionArea>
+                    </Card> :
+
+                    <Card sx={{ width: '100%' }}>
+                    <CardActionArea onClick={openModal}>
+                        <CardContent>
+                            <Typography variant='h6' component='header' gutterBottom>{title}</Typography>
+                            <Typography height='10rem' overflow='hidden'>{text}</Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button onClick={openModal}>Edit</Button>
+                        <Button>Delete</Button>
+                    </CardActions>
+                </Card> 
+            }
+
+            <Dialog open={open} onClose={closeModal} fullWidth>
+                <DialogTitle>Create new note</DialogTitle>
+                <DialogContent>
+                    <TextField 
+                        variant='filled' 
+                        label='Title' 
+                        margin='normal' 
+                        defaultValue={title}
+                        fullWidth 
+                    />
+                    <TextField 
+                        multiline 
+                        variant='filled' 
+                        label='Note' 
+                        margin='normal' 
+                        rows={10}
+                        defaultValue={text}
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={closeModal}>Cancel</Button>
+                    <Button variant='contained'>Save</Button>
+                </DialogActions>
+            </Dialog>
+
         </Fragment>
     )
 
