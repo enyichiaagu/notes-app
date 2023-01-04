7e0f41ae850ffa760a79cd0a13423da2cd94d185
enyichiaagu <enyichiaagu@gmail.com>
2023-01-04 17:45:19 +0100

    Made note card components responsive

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -15,7 +15,9 @@ function NoteCard({ add, title='', text='' }) {
         setContent({ ...content, [name]: value })
     }
 
-    useEffect(() => console.log(content), [content])
+    function saveNote() {
+        console.log(content)
+    }
 
     return (
         <Fragment>
@@ -23,8 +25,8 @@ function NoteCard({ add, title='', text='' }) {
             {
                 (add) ?
 
-                    <Card sx={{ width: '100%', height: '100%', textAlign: 'center' }}>
-                        <CardActionArea sx={{ height: '100%' }} onClick={openModal}>
+                    <Card sx={{ width: '100%', height: '18rem' }}>
+                        <CardActionArea sx={{ height: '100%', textAlign: 'center' }} onClick={openModal}>
                             <SvgIcon color='primary' sx={{ fontSize: '6rem' }}>
                                 <path d="M0 0h24v24H0z" fill="none"/><path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
                             </SvgIcon>
@@ -33,18 +35,18 @@ function NoteCard({ add, title='', text='' }) {
                         </CardActionArea>
                     </Card> :
 
-                    <Card sx={{ width: '100%' }}>
-                    <CardActionArea onClick={openModal}>
-                        <CardContent>
-                            <Typography variant='h6' component='header' gutterBottom>{content.title}</Typography>
-                            <Typography height='10rem' overflow='hidden'>{content.text}</Typography>
-                        </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-                        <Button onClick={openModal}>Edit</Button>
-                        <Button>Delete</Button>
-                    </CardActions>
-                </Card> 
+                    <Card sx={{ width: '100%', height: '18rem', position: 'relative' }}>
+                        <CardActionArea onClick={openModal}>
+                            <CardContent>
+                                <Typography variant='h6' component='header' gutterBottom>{content.title}</Typography>
+                                <Typography height='10rem' overflow='hidden'>{content.text}</Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions sx={{ position: 'absolute', bottom: 0 }}>
+                            <Button onClick={openModal}>Edit</Button>
+                            <Button>Delete</Button>
+                        </CardActions>
+                    </Card> 
             }
 
             <Dialog open={open} onClose={closeModal} fullWidth>
@@ -73,7 +75,7 @@ function NoteCard({ add, title='', text='' }) {
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={closeModal}>Cancel</Button>
-                    <Button variant='contained'>Save</Button>
+                    <Button variant='contained' onClick={saveNote}>Save</Button>
                 </DialogActions>
             </Dialog>
 
