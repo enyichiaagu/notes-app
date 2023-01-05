514e8d0aa57540c28ca86837ac874d7fac7a9ea0
enyichiaagu <enyichiaagu@gmail.com>
2023-01-05 08:02:16 +0100

    Refactored codebase

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from 'react'
+import { Fragment, useState } from 'react'
 
 import { Button, Card, CardActionArea, CardActions, CardContent, Dialog, DialogActions, DialogContent, DialogTitle, SvgIcon, TextField, Typography } from '@mui/material'
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState, useMemo } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 
 import { Typography, Grid } from '@mui/material'
 
@@ -8,7 +8,7 @@ function Home() {
 
     const [ data, setData ] = useState([])
 
-    const mockFetchedData = [
+    const mockData = [
         { id: 1, title: "New Beginnings", text: "It's the start of a new year and I'm feeling refreshed and ready to take on whatever comes my way. I spent the holiday season with my family and it was so nice to relax and spend quality time with them. Now that the holidays are over, I'm feeling motivated and ready to tackle my goals for the year. I'm excited to see what the future holds and I'm determined to make the most of it." },
         { id: 2, title: "Reflections on the Past Year", text: "As I look back on the past year, I can't help but feel grateful for all of the experiences and lessons that I've had. There were certainly some challenges, but I feel like I've grown so much as a person because of them. I've made some new friends and strengthened my relationships with others. I've also learned a lot about myself and what I want out of life. I'm looking forward to applying these lessons in the new year." },
         { id: 3, title: "Setting Goals", text: "One of my resolutions for the new year is to focus on setting and achieving specific goals. I find that when I have clear goals in mind, I'm more motivated and productive. This year, I want to set both short-term and long-term goals for myself. Some of my short-term goals include exercising regularly and saving money. My long-term goals include furthering my education and eventually starting my own business. I'm excited to see what I can accomplish with a clear plan in place." },
@@ -17,7 +17,7 @@ function Home() {
 
     function getNotes() {
         // Calling API
-        setData(mockFetchedData)
+        setData(mockData)
     }
 
     useEffect(() => {
